348f4e12677355214e83581298d4962fd6781df6
Kaars <niels.kaars@sogeti.com>
2025-05-28 14:02:33 +0200

    Fixed connection issues

--- 3d-visualization-comparison/js/common.js
+++ 3d-visualization-comparison/js/common.js
@@ -7,7 +7,7 @@
 const DittoAPI = {
     // Default settings
     settings: {
-        baseUrl: 'http://54.217.116.62:8080', // Changed from localhost to backend EC2 IP
+        baseUrl: 'http://54.217.116.62:8080', // Direct connection to backend
         thingId: 'org.eclipse.ditto:Factory', // Always use Factory as the default
         username: 'ditto',
         password: 'ditto',
